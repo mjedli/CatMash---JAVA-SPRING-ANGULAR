f60da9bd46526b729cd0b0c50487de2298ed713e
mjedli <jedli.mejdi.recup@gmail.com>
2018-11-02 16:37:25 +0100

    clean code

--- catmash-angular/src/app/catmash/catmash.component.ts
+++ catmash-angular/src/app/catmash/catmash.component.ts
@@ -11,55 +11,54 @@ import { CatMashService } from '../service/catmash.service';
 
 export class CatMashComponent implements OnInit {
 
-  constructor(private catMashService: CatMashService) { }
+    constructor(private catMashService: CatMashService) { }
 
-  private catMashUrl = 'catmash';
+    private catMashUrl = 'catmash';
 
-  Cats : RootObject[];
-  firstCatImage : RootObject;
-  secondCatImage : RootObject;
+    Cats : RootObject[];
+    firstCatImage : RootObject;
+    secondCatImage : RootObject;
 
-  /* update socre of cat*/
-  myFavorit(id:string, url:string) {
-    try {
-      this.catMashService.updateScore(id, url).toPromise().then(x => {},
-      error => {
-          console.log(error, "error");
-      }).then(x => {
-        window.location.replace(this.catMashUrl);
-      });
-    } catch (e) {
-      console.log(e);
-    }   
-  }
-
-  /* get all cats */
-  getCats(): void {
-    this.catMashService.getCats().subscribe(Cats => this.Cats = Cats);
-  }
-
-  /* inti component, get all cats and select two cats */
-  ngOnInit() {
-    try {
-      this.catMashService.getCats().toPromise().then(Cats => {
-          this.Cats = Cats;
-      },
-      error => {
-          console.log(error, "error");
-      }).then(x => {
-        var firstCatNumber = Math.floor(Math.random() * this.Cats.length);
-        var secondCatNumber = Math.floor(Math.random() * this.Cats.length);
-
-        while(firstCatNumber == secondCatNumber) {
-          secondCatNumber = Math.floor(Math.random() * this.Cats.length);
-        }
-
-        this.firstCatImage = this.Cats[firstCatNumber];
-        this.secondCatImage = this.Cats[secondCatNumber];
-      });
-    } catch (e) {
-      console.log(e);
+    /* update socre of cat*/
+    myFavorit(id:string, url:string) {
+        try {
+            this.catMashService.updateScore(id, url).toPromise().then(x => {},
+            error => {
+                console.log(error, "error");
+            }).then(x => {
+                window.location.replace(this.catMashUrl);
+            });
+        } catch (e) {
+            console.log(e);
+        }   
     }
-  }
 
+    /* get all cats */
+    getCats(): void {
+        this.catMashService.getCats().subscribe(Cats => this.Cats = Cats);
+    }
+
+    /* inti component, get all cats and select two cats */
+    ngOnInit() {
+        try {
+            this.catMashService.getCats().toPromise().then(Cats => {
+                this.Cats = Cats;
+            },
+            error => {
+                console.log(error, "error");
+            }).then(x => {
+                var firstCatNumber = Math.floor(Math.random() * this.Cats.length);
+                var secondCatNumber = Math.floor(Math.random() * this.Cats.length);
+
+                while(firstCatNumber == secondCatNumber) {
+                    secondCatNumber = Math.floor(Math.random() * this.Cats.length);
+                }
+
+                this.firstCatImage = this.Cats[firstCatNumber];
+                this.secondCatImage = this.Cats[secondCatNumber];
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
